b94745aa8937ad4b3df2a9ae0923bba717274c3c
xvdp <xvdpahlen@gmail.com>
2022-10-28 10:34:59 -0700

    added sortkey to get_files, linting fileio

--- koreto/fileio.py
+++ koreto/fileio.py
@@ -10,7 +10,7 @@ methods to deal with files
     reversedict() # key with subkey
 
 """
-from typing import Union, Any, Collection
+from typing import Union, Any, Collection, Optional
 import os
 import os.path as osp
 import json
@@ -26,19 +26,29 @@ if WITH_TORCH:
 else:
     Vector = np.ndarray
 
-def get_files(folder: Union[str, list, tuple]=".", ext: Union[str, list, tuple]=None,
-              recursive: bool=False) -> list:
+
+def get_files(folder: Union[str, list, tuple] = ".",
+              ext: Union[str, list, tuple] = None,
+              recursive: bool = False,
+              sortkey: Optional[str] = None) -> list:
     """ conditional file getter
+
     Args
         folder      (str|list ['.'])  folder | list of folders
         ext         (str|list [None]) file extensions, default, any
-        recursive   (bool[False])
+        recursive   (bool [False])
+        sortkey     (str [None]) None: alphabetically | 'mtime', 'ctime', 'atime', 'size'
+
+    Examples:
+    # return all .flac and .wav files sorted by modification time in current folder
+    >>> get_files(ext=('.flac', '.wav'), sortkey='mtime')
+    # return all .png files children to home folder recursively sorted by size
+    >>> get_files(folder='~', ext='.png', sortkey='size')
     """
     folder = [folder] if isinstance (folder, str) else folder
     folder = [osp.abspath(osp.expanduser(f)) for f in folder]
     ext = [ext] if isinstance (ext, str) else ext
     cond = lambda x, ext: True if ext is None else osp.splitext(x)[-1].lower() in ext
-
     out = []
     for fold in folder:
         if not recursive:
@@ -46,28 +56,35 @@ def get_files(folder: Union[str, list, tuple]=".", ext: Union[str, list, tuple]=
         else:
             for root, _, files in os.walk(fold):
                 out += [osp.join(root, name) for name in files if cond(name, ext)]
-    return sorted(out)
+    key = {} if sortkey is None else {'key':osp.__dict__[f'get{sortkey}']}
+    return sorted(out, **key)
 
-def verify_image(name: str, verbose: bool=False) -> bool:
+
+def verify_image(name: str, verbose: bool = False) -> bool:
     """
     True:  48 us
     False: 68 us
     jpg and png headers with open('rb') is ~3x to 6x faster, but this is practical and tested
     """
     try:
-        im = Image.open(name)
+        Image.open(name)
         return True
     except:
         if verbose:
             print(f" Not an Image: {name}")
         return False
 
-def verify_images(images: list, verbose: bool=False) -> list:
+
+def verify_images(images: list, verbose: bool = False) -> list:
     """ check that image list can be opened with PIL"""
     return [im for im in images if verify_image(im, verbose)]
 
-def get_images(folder: Union[str, list, tuple]=".", recursive: bool=False,
-               verify: bool=True, verbose: bool=True) -> list:
+
+def get_images(folder: Union[str, list, tuple] = ".",
+               recursive: bool = False,
+               verify: bool = True,
+               sortkey: Optional[str] = None,
+               verbose: bool = True) -> list:
     """ conditional image file getter
     Args
         folder      (str|list ['.'])  folder | list of folders
@@ -75,10 +92,11 @@ def get_images(folder: Union[str, list, tuple]=".", recursive: bool=False,
         verify      (bool [True]), leverages PIL to read the header of each file
                         may be a bit slower
                         loading verify_image2() should be faster but is not fully tested
+        sortkey     (str [None]) None: alphabetically | 'mtime', 'ctime', 'atime', 'size'
         verbose     (default [True])
     """
     _images = ['.jpg', '.jpeg', '.png', '.ppm', '.bmp', '.pgm', '.tif', '.tiff', '.webp']
-    out = get_files(folder=folder, ext=_images, recursive=recursive)
+    out = get_files(folder=folder, ext=_images, recursive=recursive, sortkey=sortkey)
 
     if verify:
         out = verify_images(out, verbose)
@@ -87,7 +105,8 @@ def get_images(folder: Union[str, list, tuple]=".", recursive: bool=False,
         print(f"get_images()-> {len(out)} found")
     return out
 
-def rndlist(inputs: Union[list, tuple, Vector], num: int=1) -> Any:
+
+def rndlist(inputs: Union[list, tuple, Vector], num: int = 1) -> list:
     """ returns random subset from list
     Args
         inputs   (iterable)
@@ -102,8 +121,11 @@ def rndlist(inputs: Union[list, tuple, Vector], num: int=1) -> Any:
 #
 # hashing utils
 #
-def hash_folder(files: Union[str,list], metadata: Collection=None,
-                metakey: str="metadata", save: str=None, update: dict=None) -> dict:
+def hash_folder(files: Union[str, list],
+                metadata: Optional[Collection] = None,
+                metakey: str = "metadata",
+                save: str = None,
+                update: dict = None) -> dict:
     """
         Args
             files       folder name or file list
@@ -139,8 +161,11 @@ def hash_folder(files: Union[str,list], metadata: Collection=None,
                 pickle.dump(out, _fi)
     return out
 
-def hash_file(filename: str, splitname: bool=False,
-              metadata: Any=None, metakey: str="metadata") -> dict:
+
+def hash_file(filename: str,
+              splitname: bool = False,
+              metadata: Any = None,
+              metakey: str = "metadata") -> dict:
     """
     hash dictionary
         name:       filename [if splitname: fullname]
@@ -168,7 +193,8 @@ def hash_file(filename: str, splitname: bool=False,
 # def check_file(filename, hashdic):
 #     pass
 
-def reversedict(dic: str, subkey:str, sort: bool=False) -> list:
+
+def reversedict(dic: str, subkey: str, sort: bool = False) -> list:
     """ returns (subkeyvalue, key)
     given {key_0:{<subkey>:keyval0_i, ..., subkey_n:keyval0_n},
            ...,
@@ -190,6 +216,7 @@ def reversedict(dic: str, subkey:str, sort: bool=False) -> list:
     if sort:
         out = sorted(out, key=lambda x: x[0])
     return out
+
 
 def get_keys_with_subkeyval(dic: dict, subkey: str, subval: str) -> dict:
     """ return dic subset with subkey:subval
